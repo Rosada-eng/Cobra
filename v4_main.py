959e78a04db9aa277d7203dd3f24262e8fdd523e
Rosada-eng <guilhermer4@al.insper.edu.br>
2020-06-14 22:32:44 -0300

    Melhorado o HUD

--- v4_main.py
+++ v4_main.py
@@ -68,16 +68,17 @@ class Game:
         pygame.display.set_caption ('Teste Tiled Map') #muda o título da screen
         pygame.key.set_repeat(500,100) # Inicia a função de repetir (tempo de espera, tempo para repetir cada ação)
         self.load_data()
-        self.last_hit = 0 # último hit do pássaro na cobra
+        #self.last_hit = 0 # último hit do pássaro na cobra
         self.ANALISE = True # analisa múltiplos hits do pássaro na cobra
 
         self.playing = True
         self.mostrador = " "
-        self.tempo_fase = 5000
+        self.tempo_fase = FASE1
         self.last_sec = 0
         self.Fase1 = Fase1
         self.Fase2 = Fase2
         self.Fase3 = Fase3
+        self.player_level = 1
 
         self.last_spawn = 0
         self.paused = False
@@ -99,9 +100,9 @@ class Game:
             self.map_img = self.map.make_map()
             self.map_rect = self.map_img.get_rect()
 
-        self.see_img = pygame.image.load(path.join(IMG_DIR, 'see.jpg')).convert_alpha()
+        self.see_img = pygame.image.load(path.join(IMG_DIR, 'now_you_see.png')).convert_alpha()
         self.see_img = pygame.transform.scale(self.see_img, (30,30))
-        self.not_see_img = pygame.image.load(path.join(IMG_DIR, 'not see.jpg')).convert_alpha()
+        self.not_see_img = pygame.image.load(path.join(IMG_DIR, 'now_you_dont.png')).convert_alpha()
         self.not_see_img = pygame.transform.scale(self.not_see_img, (30,30))
 
         # --- Cobra ---
@@ -200,7 +201,7 @@ class Game:
         # ==== FONTS ====
         self.alagard = path.join(FONT_DIR, 'alagard.TTF')
         self.romulus = path.join(FONT_DIR, 'romulus.TTF')
-        
+        # ==== Telas ====
         self.cortina_screen = pygame.Surface(self.screen.get_size()).convert_alpha()
         self.cortina_screen.fill((210,105,30, 170))
         self.gameover_screen = pygame.Surface(self.screen.get_size()).convert_alpha()
@@ -361,13 +362,11 @@ class Game:
         # if hits:
         #     self.player.posic += vect (BIRD_KNOCKBACK, 0).rotate(90)
 
-        
-        
 
-       
                
     def draw(self):
         self.screen.blit(self.map_img, self.camera.apply_rect(self.map_rect))
+        # -- Blit cada sprite:
         for sprite in self.all_sprites: #Analisa cada um dos sprites do grupo e mandar imprimir
             # -- Pássaro --
             # if isinstance (sprite, CrazyBirds):
@@ -375,26 +374,41 @@ class Game:
             # -- Fruta --
             if isinstance (sprite, Fruit): # se for relacionado a classe Fruit
                 quad_dist_to_player = (self.player.posic.x - sprite.pos.x)**2 + (self.player.posic.y - sprite.pos.y)**2
-                if quad_dist_to_player <= PLAYER_VISION**2: #spawna a fruta somente se ela estiver dentro do alcance da visão
+                if quad_dist_to_player <= PLAYER_VISION**2: # Exibe a fruta somente se ela estiver dentro do alcance da visão
                     self.screen.blit(sprite.image, self.camera.apply(sprite))
             else:
                 self.screen.blit(sprite.image, self.camera.apply(sprite))
-        health_player_bar(self.screen, 10, 10, self.player.health / PLAYER_HEALTH)
-        stamine_player_bar (self.screen, 10, 30, self.player.stamine / SNAKE_MAX_STAMINE)
-        poison_charge_bar (self.screen, 10, 45, self.player.charge)
+        
+        # --- HUD ---
+        background = pygame.Surface ((190, 90)).convert_alpha()
+        background.fill ((250, 215, 160))
+        self.screen.blit(background, (5,5))
+        # - HP:
+        self.draw_text ("HP:", self.romulus, 20, BLACK, 20, 20)
+        health_player_bar(self.screen, 40, 10, self.player.health / PLAYER_HEALTH)
+        # - SP:
+        self.draw_text ("SP:", self.romulus, 20, BLACK, 20, 35)
+        stamine_player_bar (self.screen, 40, 30, self.player.stamine / SNAKE_MAX_STAMINE)
+        # - Poison cooldown:
+        poison_charge_bar (self.screen, 40, 45, self.player.charge)
+        # - LVL:
+        self.draw_text ("Lvl:", self.romulus, 20, BLACK, 20, 70)
+        self.draw_text ("{}".format(self.player_level), self.romulus, 40, RED, 50,70 )
+        # - Símbolo de Visibilidade
         if self.player.INGRASS:
-            self.screen.blit(self.not_see_img, (10,60))
+            self.screen.blit(self.not_see_img, (160,60))
         else:
-            self.screen.blit(self.see_img, (10,60))
+            self.screen.blit(self.see_img, (160,60))
 
+        # -- PAUSE --
         if self.paused:
             self.screen.blit(self.cortina_screen, (0,0))
             self.draw_text("PAUSE", self.romulus, 80, (75,0,130), WIDTH/2, HEIGHT/2)
-        
+        # -- GAME OVER -- 
         if self.GAMEOVER:
             self.screen.blit(self.gameover_screen, (0,0))
             self.draw_text("GAME OVER!", self.romulus, 80, (149,165,166), WIDTH/2, HEIGHT/2)
-        
+        # -- Timer --
         self.draw_text(self.timer(), self.romulus, 40, WHITE, WIDTH/2 , 20)
         pygame.display.flip()
 
