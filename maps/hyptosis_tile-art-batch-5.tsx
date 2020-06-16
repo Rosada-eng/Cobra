dcd892178b8e3d7a2b8b55bbb6c27bb9e939d848
Rosada-eng <guilhermer4@al.insper.edu.br>
2020-06-15 21:40:54 -0300

    adicionado música na tela inicial e retirado arquivos nao utilizados

--- maps/hyptosis_tile-art-batch-5.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="2020.05.07" name="hyptosis_tile-art-batch-5" tilewidth="15" tileheight="15" tilecount="3402" columns="42">
- <image source="../../Downloads/Good Tiles/hyptosis_tile-art-batch-5.png" trans="ff00ff" width="636" height="1219"/>
-</tileset>
